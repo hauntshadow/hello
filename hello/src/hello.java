c7abadd63f0f15e2d8ed533bc8c9b53213e6ee8e
smithc4 <smithc4@appstate.edu>
2016-01-27 13:12:39 -0500

    Works with one or zero arguments

--- hello/src/hello.java
+++ hello/src/hello.java
@@ -1,6 +1,19 @@
+/**
+ * hello.java
+ * 
+ * @author smithc4
+ * @version 1.27.16
+ */
 
 public class hello {
 	public static void main(String[] args) {
-		System.out.println("Hello erryone.");
+		if(args.length > 0)
+		{
+			System.out.println("Hello " + args[0] + ".");
+		}
+		else
+		{
+			System.out.println("Hello Chris.");
+		}
 	}
 }
