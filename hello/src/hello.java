ea1ea0bd11b3bacc0e14e19316d39c9c3e1579a6
smithc4 <smithc4@appstate.edu>
2016-01-29 13:07:14 -0500

    Initial test file. No test bodies done yet.

--- hello/src/hello.java
+++ hello/src/hello.java
@@ -9,8 +9,8 @@ public class hello {
 	public static void main(String[] args) {
 		if(args.length == 2)
 		{
-			args[0] = Character.toUpperCase(args[0].charAt(0)) + args[0].substring(1);
-			args[1] = Character.toUpperCase(args[1].charAt(0)) + args[1].substring(1);
+			args[0] = Character.toUpperCase(args[0].charAt(0)) + args[0].substring(1).toLowerCase();
+			args[1] = Character.toUpperCase(args[1].charAt(0)) + args[1].substring(1).toLowerCase();
 			System.out.println("Hello " + args[1] + ". I am " + args[0] + ". How are you today?");
 		}
 		else if(args.length == 1)
---
+++ hello/src/helloTest.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class helloTest {
+
+	@Test
+	public void testNoArg() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testOneArg() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testOneArg2() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testTwoArg() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testTwoArg2() {
+		fail("Not yet implemented");
+	}
+
+}
