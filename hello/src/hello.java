26314c83a995f5cb7435b26401fa4c32b0acbf36
Chris <smithc4@appstate.edu>
2016-01-30 20:03:07 -0500

    Test worked, and case closed.

--- hello/src/hello.java
+++ hello/src/hello.java
@@ -7,20 +7,20 @@
 
 public class hello {
 	public static void main(String[] args) {
+		printHello(args);
+	}
+	public static String printHello(String[] args) {
 		if(args.length == 2)
 		{
 			args[0] = Character.toUpperCase(args[0].charAt(0)) + args[0].substring(1).toLowerCase();
 			args[1] = Character.toUpperCase(args[1].charAt(0)) + args[1].substring(1).toLowerCase();
-			System.out.println("Hello " + args[1] + ". I am " + args[0] + ". How are you today?");
+			return ("Hello " + args[1] + ". I am " + args[0] + ". How are you today?");
 		}
 		else if(args.length == 1)
 		{
 			args[0] = Character.toUpperCase(args[0].charAt(0)) + args[0].substring(1);
-			System.out.println("Hello " + args[0] + ".");
+			return ("Hello " + args[0] + ".");
 		}
-		else
-		{
-			System.out.println("Hello Chris.");
-		}
+		return ("Hello Chris.");
 	}
 }
--- hello/src/helloTest.java
+++ hello/src/helloTest.java
@@ -2,37 +2,52 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-
+/**
+ * helloTest.java
+ * 
+ * @author smithc4
+ * @version 1.27.16
+ */
 public class helloTest {
-
 	@Test
 	public void testNoArg() {
 		String ans = "Hello Chris.";
-		assertEquals("No arg test failed", ans, main());
+		assertEquals("No arg test failed", ans, hello.printHello(new String[]{}));
 	}
 	
 	@Test
 	public void testOneArg() {
 		String ans = "Hello Human.";
-		assertEquals("One arg test failed", ans, main("Human"));
+		assertEquals("One arg test failed", ans, hello.printHello(new String[]{"Human"}));
 	}
 	
 	@Test
 	public void testOneArg2() {
 		String ans = "Hello People.";
-		assertEquals("One arg 2 test failed", ans, main("People"));
+		assertEquals("One arg 2 test failed", ans, hello.printHello(new String[]{"People"}));
 	}
 	
 	@Test
 	public void testTwoArg() {
-		String ans = "Hello Person.  I am Computo.  How are you today?";
-		assertEquals("Two arg test failed", ans, main("Computo", "Person"));
+		String ans = "Hello Person. I am Computo. How are you today?";
+		assertEquals("Two arg test failed", ans, hello.printHello(new String[]{"Computo", "Person"}));
 	}
 	
 	@Test
 	public void testTwoArg2() {
-		String ans = "Hello Minions.  I am Leader.  How are you today?";
-		assertEquals("Two arg 2 test failed", ans, main("Leader", "Minions"));
+		String ans = "Hello Minions. I am Leader. How are you today?";
+		assertEquals("Two arg 2 test failed", ans, hello.printHello(new String[]{"Leader", "Minions"}));
 	}
-
+	
+	@Test
+	public void testCorrectCase(){
+		String ans = "Hello Correctcase.";
+		assertEquals("Correct case test failed", ans, hello.printHello(new String[]{"correctcase"}));
+	}
+	
+	@Test
+	public void testCorrectCase2(){
+		String ans = "Hello Correct. I am Case. How are you today?";
+		assertEquals("Correct case test 2 failed", ans, hello.printHello(new String[]{"cASE", "cORRECT"}));
+	}
 }
